f72a95ee8d77c03052b71eefeff414104f5f050e
Antonio Wang <notreallyeight@gmail.com>
2025-08-31 23:08:53 +0200

    refactor: add responsive layout to mobile devices in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,13 @@ import { FaInstagram, FaTelegram, FaWhatsapp, FaYoutube } from "react-icons/fa";
 import Logo from "./Logo";
 
 const Footer = () => (
-  <div className="flex flex-col space-y-4 bg-lighterBlack px-8 pb-24 pt-8">
+  <div className="flex flex-col space-y-8 bg-lighterBlack px-8 pb-24 pt-8">
     <div className="flex flex-row items-center justify-between">
       <div className="flex flex-col space-y-8 text-white">
-        <div className="font-family-secondary text-3xl">CONTATTACI</div>
-        <div className="font-family-regular space-y-4 text-xl font-light">
+        <div className="font-family-secondary text-2xl lg:text-3xl">
+          CONTATTACI
+        </div>
+        <div className="font-family-regular space-y-4 text-base font-light lg:text-xl">
           <div>fotografiframe@gmail.com</div>
           <div>+39 350 566 4114</div>
           <div>+39 380 210 4819</div>
@@ -21,7 +23,7 @@ const Footer = () => (
     {/* Horizontal Separator */}
     <span className="h-px w-full bg-white" />
 
-    <div className="flex flex-row items-center justify-between">
+    <div className="flex flex-col items-center justify-between gap-4 lg:flex-row lg:gap-0">
       <div className="font-family-regular text-xl font-light text-white">
         P.IVA 03164150645
       </div>
