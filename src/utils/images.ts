b6c7164de5ae6d06bc3c0e6b888b9ceb9f019e38
Antonio Wang <notreallyeight@gmail.com>
2025-11-11 09:22:31 +0100

    feat: add production video demo thumbnail

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -65,6 +65,7 @@ import BirthdaysCarouselsImage18 from "../../public/assets/18-Angela-349-scaled.
 import BirthdaysCarouselsImage19 from "../../public/assets/18-Angela-343-scaled.jpg";
 import BirthdaysCarouselsImage20 from "../../public/assets/18-Angela-261-scaled.jpg";
 import BirthdaysServicesWeOfferImage from "../../public/assets/Tavola-da-disegno-1-1-960x245.png";
+import ProductionVideoDemoThumbnail from "../../public/assets/demo-bg-production-video.png";
 
 const images = {
   aboutUs: AboutUsImage,
@@ -109,6 +110,7 @@ const images = {
     introduction: BirthdaysIntroductionImage,
     servicesWeOffer: BirthdaysServicesWeOfferImage,
   },
+  demoProductionThumbnail: ProductionVideoDemoThumbnail,
   event: [
     EventsCarousel1,
     EventsCarousel2,
