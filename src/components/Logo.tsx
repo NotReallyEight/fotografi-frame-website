1cf124c2000a004239a6f301d5cbdedc1ba5b04f
Antonio Wang <notreallyeight@gmail.com>
2025-11-11 09:21:25 +0100

    feat: add more properties to Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,21 +1,31 @@
 import Image from "next/image";
 import images from "@/utils/images";
 import Link from "next/link";
+import ConditionalWrapper from "./ConditionalWrapper";
 
 type Props = {
   dark?: boolean;
+  redirects?: boolean;
+  bigger?: boolean;
 };
 
-const Logo = ({ dark = false }: Props) => (
-  <Link href="/" title="Fotografi Frame">
+const Logo = ({ dark = false, redirects = true, bigger = false }: Props) => (
+  <ConditionalWrapper
+    condition={redirects}
+    wrapper={(children) => (
+      <Link href="/" title="Fotografi Frame">
+        {children}
+      </Link>
+    )}
+  >
     <Image
       src={dark ? images.logo.black : images.logo.white}
       alt="Frame Logo"
-      width={125}
-      height={125}
-      className="max-h-20 max-w-20 xl:max-h-24 xl:max-w-24"
+      width={540}
+      height={540}
+      className={`${bigger ? `max-h-halfWidth max-w-halfWidth lg:max-h-halfHeight lg:max-w-halfHeight` : "max-h-20 max-w-20 xl:max-h-24 xl:max-w-24"}`}
     />
-  </Link>
+  </ConditionalWrapper>
 );
 
 export default Logo;
