b3a7705189be0d54da03ac71bf268b41f7c5851f
Antonio Wang <--edit>
2026-05-07 19:48:36 +0200

    style: format maintenance page JSX for improved readability

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,6 +1,15 @@
 export default async function MaintenancePage() {
   return (
-    <div className="text-white font-family-secondary h-dvh items-center justify-center text-center flex">
+    <div
+      className={`
+        text-white
+        font-family-secondary
+        h-dvh
+        items-center justify-center
+        text-center
+        flex
+      `}
+    >
       <div>Sito in lavorazione.</div>
     </div>
   );
